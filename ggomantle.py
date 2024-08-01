e8de7d207a298a5563944d2068fb5c0414d6a317
김현중/(협력사) 연구원/CI Unit(hyunjoong96.kim@lgepartner.com) <hyunjoong96.kim@lgepartner.com>
2024-08-01 16:49:23 +0900

    [20240801] modified ggomantle file
    Improved browser auto-shutdown

--- ggomantle.py
+++ ggomantle.py
@@ -1,57 +1,71 @@
+#########################################################################################################
 import requests
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import random
 
-# <-- 웹 크롤링 -->
+# <--------------------------- 웹 크롤링 --------------------------->
 url = "https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Korean_5800"
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
 data  = requests.get(url, headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
-# <-------------->
 
+
+
+# <------------ 해당 단어들 가져오기 및 리스트에 넣어두기 ------------>
 wordList = []
 
-# <--- 해당 단어들 가져오기 및 리스트에 넣어두기 --->
-divs = soup.find_all("span", "Kore")
-for div in divs:
-    wordList.append(div.text)
-    #print(div.text)
+spans = soup.find_all("span", "Kore")
+for span in spans:
+    wordList.append(span.text)
 
-#print(wordList)       # 리스트 내용 출력
-#print(len(wordList))  # 5467 개
-#<-------------------------------------------------->
-
-# 일단은 while문으로 창 꺼지는 현상 방지함.
-# 컴파일 종료하면 창도 같이 꺼져버림 -> 유지하는 방법 따로 생각..
-
-def ggomantle_auto():
-    driver = webdriver.Chrome()
-    driver.get('https://semantle-ko.newsjel.ly/')
-    driver.maximize_window()
-    time.sleep(3)
+random.shuffle(wordList)                    # 리스트 내용들 매번 랜덤으로 섞음
+#print(wordList)                            # 리스트 내용 출력
+#print(len(wordList))                       # 5467 개
 
 
-    search_box = driver.find_element(By.XPATH, '//*[@id="guess"]')
+# <-------------------- 브라우저 자동 종료 방지 -------------------->
+options = Options()
+options.add_experimental_option("detach", True)
 
-    for i in wordList:
-        search_box.send_keys(i)
-        search_box.send_keys(Keys.RETURN)
-    time.sleep(1)
+driver = webdriver.Chrome(options)
+driver.get('https://semantle-ko.newsjel.ly/')
+driver.maximize_window()
+time.sleep(3)
 
-    while(True):        # 창 안꺼지게 하기
-        pass
+search_box = driver.find_element(By.XPATH, '//*[@id="guess"]')
+for i in wordList:
+    search_box.send_keys(i)                 # 리스트 값 넣기
+    search_box.send_keys(Keys.RETURN)       # 엔터랑 같은 효과
+time.sleep(1)
 
-ggomantle_auto()
+
+#########################################################################################################
 
 
 
 
-# ================================== garbage code ===========================================
 
-#from selenium.webdriver.chrome.options import Options
+# 일단 아래 코드는 미사용..
+# def ggomantle_auto():
+#     driver = webdriver.Chrome()
+#     driver.get('https://semantle-ko.newsjel.ly/')
+#     driver.maximize_window()
+#     time.sleep(3)
 
-#chrome_options = Options()
-#chrome_options.add_experimental_option("detach", True)    # == 브라우저가 종료되도 세션 유지하여 다시 접속 할 수 있게 하는 기능. 
+
+#     search_box = driver.find_element(By.XPATH, '//*[@id="guess"]')
+
+#     for i in wordList:
+#         search_box.send_keys(i)
+#         search_box.send_keys(Keys.RETURN)
+#     time.sleep(1)
+
+#     while(True):        # 창 안꺼지게 하기
+#         pass
+
+#ggomantle_auto()
